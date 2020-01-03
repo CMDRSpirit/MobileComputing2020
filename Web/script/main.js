462f50215c4b51b6be05f6c647ad2d99e1056509
CommanderSpirit <spiritofpes09@gmail.com>
2020-01-03 16:12:31 +0100

    fix

--- Web/script/main.js
+++ Web/script/main.js
@@ -354,7 +354,7 @@ class Renderer{
 
 		gl.disable(gl.DEPTH_TEST);
 		gl.enable(gl.BLEND);
-		gl.blendFunc(gl.ONE, gl.ONE);
+		gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
 
 		//render pois
 		gl.useProgram(this.poi_shader_program);
